78cf6be62fd6d3e63d62c65d00bcb90100f876dd
kardi <kardi.dev@gmail.com>
2015-04-16 18:18:25 +0300

    [+] new training : restful functionality was added 2

--- src/main/java/com/kardi/test/java/training/special/restful/TestResource.java
+++ src/main/java/com/kardi/test/java/training/special/restful/TestResource.java
@@ -3,9 +3,10 @@ package com.kardi.test.java.training.special.restful;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 /**
+ * JSR311 - Restful implementation using Jersey
+ *
  * Created by m.lysenchuk on 4/16/15.
  */
 
@@ -31,4 +32,23 @@ public class TestResource {
         return bean;
     }
 
+    @GET
+     @Path("xml")
+     @Produces("application/xml")
+     public TestRestfulBean getXmlServerResponse() {
+
+        TestRestfulBean bean = new TestRestfulBean();
+        bean.name = "Name";
+        bean.number = 1999;
+
+        return bean;
+    }
+
+    @GET
+    @Path("html")
+    @Produces("text/html")
+    public String getHtmlServerResponse() {
+
+        return "<html><body>Restful response in <b>HTML</b></body></html>";
+    }
 }
